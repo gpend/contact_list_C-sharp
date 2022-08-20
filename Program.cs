ba1c44404d4fb69c102f03eb0acee2925f57d089
greg <gpendleton@gmail.com>
2022-08-20 17:08:33 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class contactListClass
+{
+    class Contact
+    {
+        
+    };
+
+    static void Main()
+    {
+        Console.WriteLine("Enter the contacts first name");
+        string key = Console.ReadLine();
+        Console.WriteLine(key);
+
+    }
+}
+
