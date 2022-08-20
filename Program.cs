2f6ce3b80f6a3290470fa8604173317126c2dd1e
greg <gpendleton@gmail.com>
2022-08-20 17:40:18 -0500

    add contact class

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-class contactListClass
+class ContactListClass
 {
     class Contact
     {
-        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumnber { get; set; }
     };
 
     static void Main()
@@ -11,6 +13,10 @@ class contactListClass
         Console.WriteLine("Enter the contacts first name");
         string key = Console.ReadLine();
         Console.WriteLine(key);
+        Contact thisContact = new Contact();
+        thisContact.FirstName = key;
+        thisContact.LastName = "lname";
+        thisContact.PhoneNumnber = "1234";
 
     }
 }
