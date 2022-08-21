c9ecdaacf5b22594d52b93fda1372c88a6aec158
greg <gpendleton@gmail.com>
2022-08-21 08:32:31 -0500

    fix contactList array

--- Program.cs
+++ Program.cs
@@ -1,23 +1,55 @@
 ﻿using System;
 class ContactListClass
 {
-    class Contact
+    public static System.Collections.ArrayList contactList = new() ;
+    public class Contact
     {
-        public string FirstName { get; set; }
+        public string FirstName = String.Empty;
         public string LastName { get; set; }
         public string PhoneNumnber { get; set; }
     };
 
     static void Main()
     {
-        Console.WriteLine("Enter the contacts first name");
-        string key = Console.ReadLine();
-        Console.WriteLine(key);
-        Contact thisContact = new Contact();
-        thisContact.FirstName = key;
-        thisContact.LastName = "lname";
-        thisContact.PhoneNumnber = "1234";
+        
+        while (true){
+            switch ((int)Console.ReadKey(false).Key)
+            {
+                case 1:
+                    AddContact();
+                    break;
 
+                /*case 2:
+                    RemoveContact();
+                    break;
+
+                case 3:
+                    ListContacts();
+                    break;
+
+                case 4:
+                    Environment.Exit(0);
+                    break; */
+
+                default:
+                    Console.WriteLine("please enter a number 1-4");
+                    break;
+            }
+        }
+        
+
+    }
+
+    public static void AddContact()
+    {
+        Contact thisContact = new();
+        Console.WriteLine("Enter the contacts first name");
+        thisContact.FirstName = Console.ReadLine();
+        Console.WriteLine("Enter the contacts last name");
+        thisContact.LastName = Console.ReadLine();
+        Console.WriteLine("Enter the contacts phone number");
+        thisContact.PhoneNumnber = (string)Console.ReadLine();
+        contactList.Add(thisContact);
     }
 }
 
